7c50ccf343e50fdfea21c10fd3331c24dc384b67
xzplore <xzplore431@gmail.com>
2025-11-24 23:21:48 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,28 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    // 🔥 هذا أهم سطر عشان GitHub Pages يشتغل
+    base: "/Task-organizer/",
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    plugins: [react()],
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
